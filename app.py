ee59062bee91f0f82b1e8cb69627f3d751a9629a
ArJun <103882280+pingArJun@users.noreply.github.com>
2023-05-14 20:39:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,156 +1,29 @@
-import time
-import datetime
-import pyttsx3
-import speech_recognition as sr
-import wikipedia
-import requests
-import json
-import webbrowser
+import streamlit as st
+import openai
 import os
-import pywhatkit as kit
-import smtplib
-author = "suneel"
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-#print(voices)
-engine.setProperty('voice',voices[1].id)
 
+# Set up OpenAI API credentials
+openai.api_key = os.environ["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"]
 
-def takecommand():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening...")
-        time.sleep(1)
-        audio = r.listen(source)
+# Set up Streamlit app
+st.title("AI Assistant")
 
-    try:
-        print("Recognizing...")
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
-    except Exception as e:
-        print(e)
-        return "None"
-    return query.lower()
+# Define function for querying OpenAI API
+def query_ai(query):
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=query,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response.choices[0].text
 
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
-    output_label.config(text=audio)
+# Add text input box for user to ask a question
+question = st.text_input("Ask me anything")
 
-def sendEmail(to,content):
-    server = smtplib.SMTP('smtp.gmail.com',587)
-    server.ehlo()
-    server.starttls()
-    server.login('your gmail address','your passsword')
-    server.sendmail('your gmail address',to,content)
-    server.close()
-
-def wishMe():
-    hour = int(datetime.datetime.now().hour)
-    if hour>=0 and hour < 12 :
-        speak(f"good Morning {author}")
-    elif hour >= 12 and hour < 18:
-        speak(f"good afternoon {author}")
-    else:
-        speak(f"Good Evening {author}")
-
-    speak(f"hello{author} I am ziara,how can i help you ")
-def takecommand():
-    
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("listening...")
-        r.pause_threshold = 0.8
-        audio = r.listen(source)
-    try:
-        print("Recognizing...")
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User Said:{query}\n")
-
-    except Exception as e:
-        print(f"sorry{author},say that again...")
-        return "None"
-    return query
-
-
-
-if __name__ == "__main__":
-    #speak(f"welcome {author},I am ziara")
-    wishMe()
-    #takecommand()
-    if 1:
-
-        query = takecommand().lower()
-        if 'wikipedia' and 'who' in query:
-            speak("searching wikipedia...")
-            query = wikipedia.summary(query,sentences=2)
-            results = wikipedia.summary(query, sentences=2)
-            speak("According to wikipedia")
-            print(results)
-            speak(results)
-            # made by ArJun 
-        elif 'news' in query:
-            speak("news Headlines")
-            query=query.replace("news","")
-            url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=5af48d5a86ef4374a5bafa0c66021f77"
-            news = requests.get(url).text
-            news = json.loads(news)
-            art =  news['articles']
-            for article in art:
-                print(article['title'])
-                speak(article['title'])
-
-                print(article['description'])
-                speak(article['description'])
-                speak("moving on the next news")
-
-        elif'open google' in query:
-            webbrowser.open("google.com")
-
-
-        elif'open youtube' in query:
-            webbrowser.open("youtube.com")
-        elif'search browser' in query:
-            speak("what should i search ?")
-            um = takecommand().lower()
-            webbrowser.open(f"{um}")
-        elif'ip address' in query:
-            ip = requests.get('http://api.ipify.org').text
-            print(f"your ip is {ip}")
-            speak(f"your ip is {ip}")
-        elif 'open chrome' in query:
-            codepath = "C:\Program Files\Google\Chrome\Application\chrome.exe" 
-            os.startfile(codepath)   
-        elif 'open command prompt' in query:
-            os.system("start cmd") 
-        elif 'play music' in query:
-            music_dir= "C:\\Users\\ajarj\\Downloads\\music"
-            songs = os.listdir(music_dir)
-            print(songs)
-            os.startfile(os.path.join(music_dir,songs[0])) 
-        elif 'play youtube' in query:
-            speak("what should i search in youtube ? ")
-            cm = takecommand().lower()
-            kit.playonyt(f"{cm}")
-        elif 'search google' in query:
-            speak("what should i search in google ? ")
-            cm = takecommand().lower()
-            kit.playonyt(f"{cm}") 
-        elif'send message' in query:
-            speak("who do you want to send a message")
-            num = input("Enter number :/n")
-            speak("what do you want to send?")
-            msg = takecommand().lower()
-            speak("please enter time sir.")
-            H = int(input("enter hour ?/n"))
-            M = int(input("enter Minutes ?/n"))
-            kit.sendwhatmsg(num,msg,H ,M)
-        elif 'send email ' in query:
-            speak("what should I send sir ?")
-            content=takecommand().lower()
-            speak("whom to send the email, enter email address sir")
-            to = input("Enter Email Address:\n")
-            sendEmail(to,content)
-
-            
-
+# If the user has entered a question, query the AI and display the answer
+if question:
+    answer = query_ai(question)
+    st.write(answer)
